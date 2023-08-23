dad07927bf2280201b586a73f1b7b9a694c9d36d
OfflineLad <mrsyahda99@gmail.com>
2023-08-23 23:39:03 +0700

    Source Code

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public KeyCode input;
+
+    private HingeJoint hinge;
+    private float targetPressed;
+    private float targetReleased;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+
+        targetPressed = hinge.limits.max;
+        targetReleased = hinge.limits.min;
+    }
+
+    void Update()
+    {
+        ReadInput();
+    }
+
+    private void ReadInput()
+    {
+        JointSpring jointSpring = hinge.spring;
+
+        if (Input.GetKey(input))
+        {
+            jointSpring.targetPosition = targetPressed;
+        }
+        else
+        {
+            jointSpring.targetPosition = targetReleased;
+        }
+
+        hinge.spring = jointSpring;
+    }
+}
